ac51401911ee2bdd013c317fcd29b71b3154db58
Jvymvn <Jordon.carter@yahoo.com>
2019-06-04 12:23:43 -0400

    drill1

---
+++ higher-order_functions.js
@@ -0,0 +1,20 @@
+'use strict';
+//Functions as arguments (1)
+function repeat (fn, n){
+    for(let i = 0; i < n; i++){
+        fn();
+    }
+}
+
+function hello(){
+    console.log("Hello world");
+}
+
+function goodbye(){
+    console.log("Goodbye world");
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
+
+//Functions as arguments (2)
